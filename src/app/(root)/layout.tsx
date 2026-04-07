265653bae1b564227d8e3b0333621a754c4c4d19
Christel Hamers <christelhamers@gmail.com>
2026-04-07 17:52:38 +0100

    make top nav sticky

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,10 +2,14 @@ import TopNav from "@/components/nav/top-nav";
 
 const LandingPageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-screen bg-background">
-      <TopNav />
-      {children}
+    <div className="min-h-screen flex flex-col bg-background">
+      <header className="sticky  top-0 z-50 border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/80">
+        <TopNav />
+      </header>
+
+      <main className="flex-1">{children}</main>
     </div>
   );
 };
+
 export default LandingPageLayout;
